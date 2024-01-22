666cd47f83ea442211467282e79fa0baf816063d
lazyloops1 <paul.hamelin1@outlook.fr>
2024-01-22 16:50:12 +0100

    titre

---
+++ script.js
@@ -0,0 +1,6 @@
+//écrire deux mots dans le canvas c1, une ave une ecriture plieinne et l'autre une ecriture vide
+
+
+
+// telecharger une image ou utiliser celle fournie et l'integrer au canvas c2
+
